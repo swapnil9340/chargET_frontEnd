11de1f9f43e6d67075ae37857b048a4020b8d556
Chandrajeet123 <selnoxreact@gmail.com>
2024-07-17 00:49:37 +0530

    login and sign up page

---
+++ src/pages/login.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Styles from '@/styles/style.module.scss'
+
+const login = () => {
+  return (
+    <div className={Styles.accespage}>
+         <div className={Styles.rightside}>
+
+</div>
+    <div className={Styles.leftside}>
+        <div className={Styles.contentbox}>
+          <h2 className={Styles.maintitle}>{`Login In`}</h2>
+          {/* <h3 className={Styles.subtitle}>{`Create Your account`}</h3>
+          <p className={Styles.para}>{`Take the next step`}</p>
+          <button className={Styles.loginwithgoolebtn}>{`Continue with Google`}</button>
+          <div class="header__center">{`Or`}</div> */}
+
+          <form>
+              <input type='email' placeholder='Email' className={Styles.inputstylelogin}/>
+              <input type='password' placeholder='Password' className={Styles.inputstylelogin}/>
+              <button className={Styles.loginwithgoolebtn}>{`Log In`}</button>
+          </form>
+          <h4 className={Styles.pagecahngebtn}>{`Forgot Your Password`}</h4>
+          <button className={Styles.loginsubmitbtn}>{`Continue with Google`}</button>
+        </div>
+    </div>
+   
+  </div>
+  )
+}
+
+export default login
---
+++ src/pages/signup.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Styles from '@/styles/style.module.scss'
+const signup = () => {
+  return (
+    <div className={Styles.accespage}>
+      <div className={Styles.leftside}>
+          <div className={Styles.contentbox}>
+            <h2 className={Styles.maintitle}>{`Sign Up`}</h2>
+            <h3 className={Styles.subtitle}>{`Create Your account`}</h3>
+            <p className={Styles.para}>{`Take the next step`}</p>
+            <button className={Styles.loginwithgoolebtn}>{`Continue with Google`}</button>
+            <div class="header__center">{`Or`}</div>
+
+            <form>
+                <input type='email' placeholder='Email' className={Styles.inputstylelogin}/>
+                <input type='password' placeholder='Password' className={Styles.inputstylelogin}/>
+                <button className={Styles.loginsubmitbtn}>{`Sign Up`}</button>
+            </form>
+            <h4 className={Styles.pagecahngebtn}>{`Already have a account? Log In`}</h4>
+          </div>
+      </div>
+      <div className={Styles.rightside}>
+
+      </div>
+    </div>
+  )
+}
+
+export default signup
