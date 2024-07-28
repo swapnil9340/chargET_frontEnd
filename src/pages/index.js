90fb698ba0ee9dd68e7250c73fc700007b0949a0
swapnil9340 <selnox93@gmail.com>
2024-07-28 21:11:41 +0530

    text field

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import dynamic from "next/dynamic";
-const Dashbord = dynamic(() => import('../component/dashboard/index'), 
-// {
-  // loading: () => <p>Loading...</p>,
-// }
-)
 
-export default function Home() {
+const Dashbord = dynamic(() => import('../component/dashboard/index'), )
+
+ function Home() {
   return (
     <>
      <Dashbord></Dashbord>
     </>
   );
 }
+
+export default Home
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,22 +2,63 @@ import React from 'react'
 import Styles from '@/styles/style.module.scss'
 import Link from 'next/link'
 import { useForm } from 'react-hook-form';
+import { makeStyles } from '@mui/styles';
+import TextField from '@mui/material/TextField';
+import { InputAdornment } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+// const useStyles = makeStyles({
+//   inputStyleLogin: {
+//     width: '100%',
+//     '& .MuiInputBase-root': {
+//       padding: '8px 8px',
+//       border: '1px solid #ededed',
+//       borderRadius: '100px',
+//       backgroundColor: '#ffffff',
+//       color: '#929292',
+//       fontSize: '16px',
+//       fontFamily: '"Work Sans", sans-serif',
+//       lineHeight: '24px',
+//       outline: 'none',
+//       marginBottom: '10px',
+//     },
+//     '& .MuiOutlinedInput-notchedOutline': {
+//       borderColor: '#ededed',
+//     },
+//     '& .MuiInputBase-input': {
+//       color: '#929292',
+//     },
+//   },
+// });
 const signup = () => {
+  // const classes = useStyles();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
 
   const Submit = async (event) => {
+
+    const data = {
+      email: event.email,
+      password: event.password
+    };
+    const url = 'https://ubgfdc54cldffk3t4f3apd7gay0pwdfp.lambda-url.us-east-1.on.aws/?type=user&action=register';
+
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email:event.email, password:event.password }),
+        body: JSON.stringify(data),
       });
-
+      console.log(response)
       if (response.ok) {
 
       } else {
@@ -25,45 +66,87 @@ const signup = () => {
       }
     } catch (error) {
       // Handle error
-      
+
     }
   };
 
   return (
     <div className={Styles.accespage}>
       <div className={Styles.leftside}>
-          <div className={Styles.contentbox}>
-            <h2 className={Styles.maintitle}>{`Sign Up`}</h2>
-            <h3 className={Styles.subtitle}>{`Create Your account`}</h3>
-            <p className={Styles.para}>{`Take the next step`}</p>
-            <button className={Styles.loginwithgoolebtn}>{`Continue with Google`}</button>
-            <div class="header__center">{`Or`}</div>
+        <div className={Styles.contentbox}>
+          <h2 className={Styles.maintitle}>{`Sign Up`}</h2>
+          <h3 className={Styles.subtitle}>{`Create Your account`}</h3>
+          <p className={Styles.para}>{`Take the next step`}</p>
+          <button className={Styles.loginwithgoolebtn}>{`Continue with Google`}</button>
+          <div class="header__center">{`Or`}</div>
 
-            <form onSubmit={handleSubmit(Submit)}>
-                <input type='email' {...register('email',  { required: true })} placeholder='Email' className={Styles.inputstylelogin}/>
-                {errors.email && <p>email is required.</p>}
-                <input type='password' 
-                 {...register('password', {
-                  required: 'Password is required',
-                  minLength: {
-                      value: 8,
-                      message: 'Password must be at least 8 characters long'
-                  },
-                  validate: {
-                      hasNumber: value =>
-                          /\d/.test(value) || 'Password must contain at least one number',
-                      hasUpperCase: value =>
-                          /[A-Z]/.test(value) || 'Password must contain at least one uppercase letter',
-                      hasSpecialChar: value =>
-                          /[!@#$%^&*(),.?":{}|<>]/.test(value) || 'Password must contain at least one special character',
-                  }
+          <form onSubmit={handleSubmit(Submit)}>
+            <input type='email' {...register('email', { required: true })} placeholder='Email' className={Styles.inputstylelogin} />
+            {errors.email && <p>email is required.</p>}
+            <TextField
+                 type={showPassword ? 'text' : 'password'}
+              placeholder="Password"
+              variant="outlined"
+              fullWidth
+              // className={classes.inputStyleLogin}
+              sx={{
+                width: '100%',
+                '& .MuiInputBase-root': {
+                  height:"48px",
+                  // padding: '8px 8px',
+                  borderRadius: '100px',
+                  backgroundColor: '#ffffff',
+                  color: '#929292',
+                  fontSize: '16px',
+                  fontFamily: '"Work Sans", sans-serif',
+                  // lineHeight: '24px',
+                  outline: 'none',
+                  // marginBottom: '10px',
+                },
+                '& .MuiOutlinedInput-notchedOutline': {
+                  border: 'none',
+                },
+                '& .MuiInputBase-input': {
+                  color: '#929292',
+                },
+                '& .MuiInputBase-input::placeholder': {
+                  color: 'black', // Customize placeholder color
+                },
+              }}
+              {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters long',
+                },
+                validate: {
+                  hasNumber: (value) =>
+                    /\d/.test(value) || 'Password must contain at least one number',
+                  hasUpperCase: (value) =>
+                    /[A-Z]/.test(value) || 'Password must contain at least one uppercase letter',
+                  hasSpecialChar: (value) =>
+                    /[!@#$%^&*(),.?":{}|<>]/.test(value) || 'Password must contain at least one special character',
+                },
               })}
-                placeholder='Password' className={Styles.inputstylelogin}/>
-                { errors.password && <p>{errors.password.message}</p>}
-                <button type='submit' className={Styles.loginsubmitbtn}>{`Sign Up`}</button>
-            </form>
-           <Link href={'/login'}> <h4 className={Styles.pagecahngebtn}>{`Already have a account? Log In`}</h4></Link>
-          </div>
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              InputProps={{ endAdornment:
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff color='red'/> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }}
+            />
+
+            <button type='submit' className={Styles.loginsubmitbtn}>{`Sign Up`}</button>
+          </form>
+          <Link href={'/login'}> <h4 className={Styles.pagecahngebtn}>{`Already have a account? Log In`}</h4></Link>
+        </div>
       </div>
       <div className={Styles.rightside}>
 
