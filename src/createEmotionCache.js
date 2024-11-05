118e92cac9dac1a4b36522fed43636b394807ecf
SelnoxAshwani <selnoxreact@gmail.com>
2024-11-05 21:56:50 +0530

    first commit from my id

--- src/createEmotionCache.js
+++ src/createEmotionCache.js
@@ -3,5 +3,5 @@ import createCache from '@emotion/cache';
 
 // Prepend: true moves MUI styles to the top of the <head> so they're loaded first.
 export default function createEmotionCache() {
-  return createCache({ key: 'css', prepend: true });
+  return createCache({ key: 'css', prepend: true }); 
 }
