1d77d14d18e08598f3a056b40c0461711a5ad813
SelnoxInfotech <SelnoxInfotech>
2024-11-07 12:43:27 +0530

    chnage

--- src/component/Mediacard/Mediacard.js
+++ src/component/Mediacard/Mediacard.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react'
 import { FaHeart , FaRegHeart } from "react-icons/fa";
 import classes from '@/styles/style.module.scss'
 
-const Mediacard = () => {
+const Mediacard = ({item}) => {
   const [like,setlike]=useState()
+
   return (
     <div className={classes.mediacard}>
        
         <div className={classes.mediacardimage}>
-            <Image width={100} height={100}  src='https://i.ibb.co/qmLJq6g/Image-1.png' alt='image'></Image>
+            <Image width={100} height={100}  src={item.asset_url}   onError={() => 'https://i.ibb.co/qmLJq6g/Image-1.png'} alt='image'></Image>
         </div>
         <h3 className={classes.mediacardimgename}>Image Name</h3>
       <div className='d-flex justify-content-between align-content-center'>
