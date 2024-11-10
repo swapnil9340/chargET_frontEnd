5db8ece92c4300ae7d1436951f34071034c1e3cc
SelnoxAshwani <selnoxreact@gmail.com>
2024-11-10 15:45:27 +0530

    weather api

--- src/component/Homesection/nameandlocation.js
+++ src/component/Homesection/nameandlocation.js
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Paper, Typography } from '@mui/material';
 import CircleIcon from '@mui/icons-material/Circle';
 import Image from 'next/image'
+import Cookies from 'js-cookie';
+import axios from 'axios'
 const NameAndLocation = () => {
+    const [weather , setweather]=useState([])
+    React.useEffect(() => {
+        // const fetchData = async () => {
+        //   const cookieValue = Cookies.get('ChargeET_UserToken');
+    
+        //   if (!cookieValue) {
+        //     console.warn("User token cookie not found.");
+        //     return;
+        //   }
+    
+        //   const options = {
+        //     method: 'GET',
+        //     url: 'https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=b63fyoBOlWB8YbJujlhwzwJp9lhgoDB4',
+         
+        //     headers: {
+        //       'Content-Type': 'application/json',
+        //       "Authorization": `Bearer ${`b63fyoBOlWB8YbJujlhwzwJp9lhgoDB4`}`
+
+        //     },
+            
+        //   };
+    
+        //   try {
+        //     const { data } = await axios.request(options);
+        //     if (data.status = 'success') {
+    
+        //       console.log(data.devices);
+        //     }
+        //     else {
+        //       setdevice([]);
+        //     }
+        //   } catch (error) {
+        //     console.error("Error fetching data:", error);
+        //   }
+        // };
+    
+    axios.get('https://api.tomorrow.io/v4/weather/realtime?location=bhopal&apikey=b63fyoBOlWB8YbJujlhwzwJp9lhgoDB4' , {
+               'Content-Type': 'application/json',
+               "Authorization": `Bearer ${`b63fyoBOlWB8YbJujlhwzwJp9lhgoDB4`}`
+    }).then((res)=>{
+        console.log(res.data.data.values.temperature)
+        setweather(res.data.data.values)
+    })
+        // fetchData();
+      }, [])
     return (
         <div style={{ padding: 16, height: "180px" }} >
             <Grid container spacing={2}>
@@ -16,7 +63,6 @@ const NameAndLocation = () => {
                 </Grid>
             </Grid>
         </div>
-
     )
 }
 const styles = {
