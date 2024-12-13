f9ceffbf91f953784cd514a4f553ee11fe62a4de
swapnil <selnox93@gmail.com>
2024-12-14 01:57:42 +0530

    dsaa

--- src/component/Header/Searchbar.js
+++ src/component/Header/Searchbar.js
@@ -3,14 +3,16 @@ import classes from '../../styles/style.module.scss'
 import { IoSearch } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
 import { FaArrowLeft } from "react-icons/fa"
-import Router from 'next/router';
+import {useRouter} from 'next/router';
 import WestIcon from '@mui/icons-material/West';
 import { IconButton, InputAdornment, TextField } from "@mui/material";
 const Header = () => {
     const [seartext, setSearchtext]= useState('')
+    const k = useRouter()
+    console.log(useRouter().pathname , k)
   return (
     <div className={classes.Header}>
-      <div><span>{<IconButton ><FaArrowLeft onClick={(e)=>{Router.push(-1)}} ></FaArrowLeft></IconButton>}</span></div>
+    {useRouter().pathname !== "/" && <div><span>{<IconButton ><FaArrowLeft onClick={(e)=>{Router.push(-1)}} ></FaArrowLeft></IconButton>}</span></div>}
         <div className={classes.searchbar}>
         {Boolean(seartext.length ===0) && <span><IoSearch /></span>  }
          <input type='text' placeholder='Search' onChange={(e)=>{setSearchtext(e.target.value)}} />
